4d6f8f079ffe4a49c4fccc5e479c0c7579b77c81
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-06-09 10:26:24 -0400

    adde if rame code

--- javascript.js
+++ javascript.js
@@ -22,11 +22,8 @@ var useriframe = "";
 var activitiesSummary = [];
 
 
-<<<<<<< modal
 
 
-=======
->>>>>>> master
 //User dropdown selection
 $(".dropdown-item").on("change",function(){
     userSelected = $(this).val();
@@ -64,7 +61,6 @@ $(".dropdown-item").on("change",function(){
    }).then(function(response){
        //this grabs the first index in the object array and gets the calories for it
        activitiesSummary = response;
-<<<<<<< modal
        console.log(response);
 
        activitiesSummary.forEach(function (value) {
@@ -76,20 +72,16 @@ $(".dropdown-item").on("change",function(){
         $('#completed0').append(entry);
        });
     });
+    //generates users iFrame
+    $("#powerbiIframe").attr("src",useriframe);
 });
 
 
 
 
 
-=======
-       console.log(activitiesSummary);
+console.log(activitiesSummary);
     
-   })
-
-   $("#powerbiIframe").attr("src",useriframe);
-})
->>>>>>> master
 
 //firebase data
 var config = {
